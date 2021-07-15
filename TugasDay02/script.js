3400baa0d4e1518bbe5c357c4c241b4f27199017
Muhamad Nur Ridwan <muhamadridwan6792@gmail.com>
2021-07-15 18:04:38 +0700

    first commit

--- TugasDay02/script.js
+++ TugasDay02/script.js
@@ -1,47 +1,69 @@
-function addData(){
-    console.log('====================================');
-    console.log("tes");
-    console.log('====================================');
-    var nomer;
-    var nama;
-    var alamat;
-
-    var nomer = $('#nomer').val();
-    var nama = $('#nama').val();
-    var alamat = $('#alamat').val();
-    console.log(nomer);
-    console.log(nama);
-    console.log(alamat);
-
-    // var button = document.querySelector('#addButton');
-    var table = document.querySelectorAll('table > tbody');
-
-    // table.innerHTML += `
-    //     <tr>
-    //       <td>${nomer}</td>
-    //       <td>${nama}</td>
-    //       <td>${alamat}</td>
-    //     </tr>
-    // `
-        var table = document.getElementsByTagName('table')[0];
-        var newRow = table.insertRow(table.rows.length);
+/* bagian slide foto 
+*/
+var slideIndex = 1;
+      showSlides(slideIndex);
       
+      function plusSlides(n) {
+        showSlides(slideIndex += n);
+      }
       
-        var cell1 = newRow.insertCell(0);
-        var cell2 = newRow.insertCell(1);
-        var cell3 = newRow.insertCell(2);
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
       
-        // tambah nilai ke dalam cell
-        cell1.innerHTML = nomer;
-        cell2.innerHTML = nama;
-        cell3.innerHTML = alamat;
+      function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}    
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+      }
 
-    $("#myModal").modal("hide");
-    clear();
-}
+      /**
+       * input data
+       */
 
-function clear() {
-    $("#nomer").val("");
-    $("#nama").val("");
-    $("#alamat").val("");
-}
+       function terimainput(){
+        var x=document.forms['biodata']['nim'].value;
+        var y=document.forms['biodata']['nama'].value;
+        var z=document.forms['biodata']['agama'].value;
+         
+        if(x==null || x=="")
+        {
+          document.getElementById("val_nim").innerHTML="<font color='red'><b>* Harap Isi Kolom NIM Dengan Benar</b></font>";
+        
+            if(y==null || y=="") {
+            document.getElementById("val_nama").innerHTML="<font color='red'><b>* Harap Isi Kolom NAMA Dengan Benar</b></font>";   
+          } else {
+          document.getElementById("val_nama").innerHTML="<font color='purple'>* Benar</font>";
+          }
+        } else {
+         
+        document.getElementById("val_nim").innerHTML="<font color='purple'>* Benar</font>";
+        
+        if(y==null || y=="") {
+          document.getElementById("val_nama").innerHTML="<font color='red'><b>* Harap Isi Kolom NAMA Dengan Benar</b></font>";   
+          } else {
+         
+            document.getElementById("val_nama").innerHTML="<font color='purple'>* Benar</font>";
+        
+            var tabel = document.getElementById("tabelinput");
+            var row = tabel.insertRow(1);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+        
+            cell1.innerHTML = x;
+            cell2.innerHTML = y;
+            cell3.innerHTML = z;
+          }
+        }
+        }
